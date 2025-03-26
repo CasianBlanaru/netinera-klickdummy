07296863f7fe43b856cd62b6ab152933fd7a23f2
Casian Blanaru <cab@tpwd.de>
2025-03-26 12:01:49 +0100

    add animation

--- components/PortraitSection.vue
+++ components/PortraitSection.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="flex flex-col items-center bg-[#F5F5F5] px-6 py-[93px] w-full min-h-[896px]">
-    <div class="flex flex-col items-center w-full max-w-[1439px]">
-      <h2 class="font-inter font-bold text-[#1E1E1E] text-[40px] sm:text-[32px] md:text-[36px] text-center leading-[150%] tracking-[-0.88px]">
+  <div class="flex flex-col items-center bg-[#F5F5F5] px-4 sm:px-6 py-12 sm:py-[93px] w-full min-h-[600px] sm:min-h-[896px]">
+    <div class="flex flex-col items-center w-full">
+      <h2 class="font-inter font-bold text-[#1E1E1E] sm:text-[32px] md:text-[40px] text-2xl text-center leading-[150%] tracking-[-0.88px]">
         Im Porträt
       </h2>
       
-      <p class="mt-[6px] max-w-[660px] font-inter font-normal text-[#1E1E1E] text-[20px] sm:text-[16px] md:text-[18px] text-center leading-[150%] tracking-[-0.22px]">
+      <p class="mt-4 sm:mt-[6px] max-w-[660px] font-inter font-normal text-[#1E1E1E] sm:text-[16px] md:text-[20px] text-base text-center leading-[150%] tracking-[-0.22px]">
         Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
       </p>
       
-      <div class="flex flex-col justify-end bg-[#E6F4FF] mt-[63px] px-[163px] sm:px-6 md:px-20 pb-[89px] sm:pb-10 rounded-lg w-full max-w-[1121px] h-[522px]">
-        <div class="font-inter font-normal text-[#1E1E1E] text-base leading-[150%] tracking-[-0.176px]">
+      <div class="flex flex-col justify-end bg-[#E6F4FF] mt-8 sm:mt-[63px] px-6 sm:px-[163px] pb-8 sm:pb-[89px] rounded-lg w-full h-[400px] sm:h-[522px]">
+        <div class="font-inter font-normal text-[#1E1E1E] text-sm sm:text-base leading-[150%] tracking-[-0.176px]">
           ODEG
         </div>
         
-        <h3 class="mt-6 font-inter font-bold text-[#1E1E1E] text-[40px] sm:text-[28px] md:text-[36px] leading-[150%] tracking-[-0.88px]">
+        <h3 class="mt-4 sm:mt-6 font-inter font-bold text-[#1E1E1E] sm:text-[28px] md:text-[40px] text-2xl leading-[150%] tracking-[-0.88px]">
           Im Osten Deutschlands unverzichtbar
         </h3>
       </div>
